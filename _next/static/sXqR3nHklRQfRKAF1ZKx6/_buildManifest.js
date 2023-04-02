0b570a7d2d54ec5b01ab3a9cf4e604ba3456d2e2
Federico Garcia <65201698+FedericoGarciaGarcia@users.noreply.github.com>
2023-04-02 10:42:01 +0900

    Change path

--- _next/static/sXqR3nHklRQfRKAF1ZKx6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-1595fcd0d52f1e2d.js","static/chunks/445-e3dbb9de3ffd5f96.js","static/chunks/pages/index-1bd35f1e6788f4b5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
